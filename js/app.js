32d849afb707b6f4de3d8826e0b47b9ce2817c02
Juliao <juliaomc@gmail.com>
2025-03-17 23:48:32 -0300

    alterando limite amigos

--- js/app.js
+++ js/app.js
@@ -1,11 +1,11 @@
-let elemAmigo = document.getElementById("nome-amigo"); //input
+let elemAmigo = document.getElementById("nome-amigo"); // input
 let elemListaAmigos = document.getElementById("lista-amigos"); // paragrafo
 let elemListaSorteio = document.getElementById("lista-sorteio"); // paragrafo
 
 let amigos = [];
 
 function adicionar() {
-  // lista de amigos desde que o campo não esteja em vazio ou já adicionado
+  // Adiciona amigos se o campo não estiver vazio ou já adicionado
   const amigo = elemAmigo.value;
   if (amigo && !amigos.includes(amigo.toUpperCase())) {
     amigos.push(amigo.toUpperCase());
@@ -13,24 +13,14 @@ function adicionar() {
     elemAmigo.value = "";
   }
 }
+
 function sortear() {
   elemListaSorteio.innerHTML = "";
-
-  embaralhaArray(amigos);
-  for (i = 0; i < amigos.length; i++) {
-    if (i == amigos.length - 1) {
-      elemListaSorteio.innerHTML += `${amigos[i]} --> ${amigos[0]}<br>`;
-    } else {
-      elemListaSorteio.innerHTML += `${amigos[i]} --> ${amigos[i + 1]}<br>`;
-    }
-  }
-}
-
-// FUNCAO PARA EMBARALHAR ARRAY
-function embaralhaArray(arr) {
-  for (let i = arr.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [arr[i], arr[j]] = [arr[j], arr[i]];
+  if (amigos.length > 0) {
+    const sorteado = amigos[Math.floor(Math.random() * amigos.length)];
+    elemListaSorteio.innerHTML = `Sorteado: ${sorteado}`;
+  } else {
+    elemListaSorteio.innerHTML = "Nenhum amigo na lista para sortear.";
   }
 }
 
